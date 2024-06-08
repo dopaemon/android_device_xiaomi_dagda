501baf1ef875922b0c2f5e716714ce71c9bb33c4
dopaemon <polarisdp@gmail.com>
2024-06-08 15:14:01 +0000

    AlphaDroid: Bringup
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- lineage_dagda.mk
+++ lineage_dagda.mk
@@ -10,25 +10,39 @@ $(call inherit-product, device/xiaomi/dagda/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
-
-# QTI Framework boost
-TARGET_SUPPORTS_FRAMEWORK_BOOST := true
-
-# RisingOS
-WITH_GMS := true
-# TARGET_CORE_GMS := true
-TARGET_ENABLE_BLUR := true
+# Device config
 TARGET_HAS_UDFPS := true
-TARGET_USE_GOOGLE_TELEPHONY := true
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-RISING_BUILDTYPE := UNOFFICIAL
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RISING_MAINTAINER="KernelPanix" \
-    RISING_CHIPSET="Snapdragon 8 Gen 1"
+# GAPPS
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+WITH_GMS := true
+TARGET_BUILD_PACKAGE := 3
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+TARGET_INCLUDE_PIXEL_LAUNCHER := false
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+# TARGET_LAUNCHER options:
+# 1 - stock (default)
+# 2 - lawnchair
+# 3 - pixel (valid only on gapps builds)
+TARGET_LAUNCHER := 2
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+
+# Maintainer
+ALPHA_BUILD_TYPE := UNOFFICIAL
+ALPHA_MAINTAINER := KernelPanix
 
 PRODUCT_NAME := lineage_dagda
 PRODUCT_DEVICE := dagda
